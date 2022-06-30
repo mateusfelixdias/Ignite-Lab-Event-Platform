86e0c1a47cb2af6cfdbc6f8a1fa24d94803966ea
mateusfelixdias <felixmateus618@gmail.com>
2022-06-30 12:58:51 -0300

    added page subscribe.

--- frontend/src/pages/Subscribe.tsx
+++ frontend/src/pages/Subscribe.tsx
@@ -1,7 +1,7 @@
 import { FormEvent, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../generated";
+import { useNavigate } from "react-router-dom";
 
 
 export function Subscribe() {
