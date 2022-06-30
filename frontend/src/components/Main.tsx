75951da7c13979615b63f969cabe31742e411f3b
mateusfelixdias <felixmateus618@gmail.com>
2022-06-30 12:57:55 -0300

    added component Main.

---
+++ frontend/src/components/Main.tsx
@@ -0,0 +1,33 @@
+import { Evento } from "../pages/Evento";
+import { ComeBack } from "./ComeBack";
+import { Frame } from "./Frame";
+import { Logo } from "./Logo";
+import { useState } from "react";
+
+export function Main() {
+  const [accessClassesAndBackButton, setAccessClassesAndBackButton] = useState<boolean>(false);
+
+  function handleaccessClassesAndBackButton() {
+    accessClassesAndBackButton
+      ? setAccessClassesAndBackButton(false)
+      : setAccessClassesAndBackButton(true);
+  };
+
+  return (
+    <>
+      <header className="py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600 mobile:justify-between mobile:p-2">
+        <Logo />
+        <div className="hidden mobile:flex">
+          <strong className="p-1">Aulas</strong>{" "}
+          <button type="button" onClick={handleaccessClassesAndBackButton}>
+            {accessClassesAndBackButton ? <ComeBack /> : <Frame />}
+          </button>
+        </div>
+      </header>
+
+      <main>
+        <Evento accessClassesAndBackButton={accessClassesAndBackButton} />
+      </main>
+    </>
+  );
+};
