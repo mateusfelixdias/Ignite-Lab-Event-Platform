3c71c58d2142dd4ecf6d37f232f5af3b0ebcc0d2
mateusfelixdias <felixmateus618@gmail.com>
2022-07-01 14:42:42 -0300

    added new routes.

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Main } from "./components/Main";
 import { Subscribe } from "./pages/Subscribe";
 
 export function Router() {
   return (
-    <HashRouter basename="/">
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Subscribe />} />
+        <Route path="/cadastro" element={<Subscribe />} />
         <Route path="/event" element={<Main />} />
         <Route path="/event/lesson/:slug" element={<Main />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
