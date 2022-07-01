145318e8e9ad546ef4ceaafbc9b57b5958c2a35e
mateusfelixdias <felixmateus618@gmail.com>
2022-07-01 15:54:01 -0300

    added new routes.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,16 @@
 import { App } from "./App";
+import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./style/global.css";
-import { BrowserRouter, HashRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+root.render(
   <BrowserRouter>
-    <App/>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </BrowserRouter>
 );
